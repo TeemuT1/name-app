6b35d3b35a979c3e1f6bfb9543c8ad83c2a4ed0a
Teemu <teemu_typpi@hotmail.com>
2021-01-20 01:25:59 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+
+const hello = () => {
+    console.log('hello from src index.js')
+}
+hello()
+ReactDOM.render(<App />, document.getElementById('root'))
